782cac43465552ad01cd6dab35b910aed49a38f2
Sirajul islam <iamsiraj13@gmail.com>
2023-06-17 12:53:41 +0600

    modal create

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,31 @@
- "use client"
+"use client";
 
 import { CustomButtonProps } from "@/types";
+import Image from "next/Image";
 
-const CustomButton = ({title, containerStyles, handleClick, btnType}:CustomButtonProps) => {
-    return (
-        <button 
-            disabled={false}
-            type={btnType || "button"}
-            className={`custom-btn ${containerStyles}`}
-            onClick={handleClick}
-        >
-           <span className={`flex-1`}>
-            {title}
-           </span>
-
-        </button>
-    );
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  textStyle,
+  rightIcon,
+  btnType,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyle}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="" fill className="object-contain ml-2" />
+        </div>
+      )}
+    </button>
+  );
 };
 
 export default CustomButton;
--- components/index.ts
+++ components/index.ts
@@ -1,16 +1,20 @@
-import Hero from "./Hero"
-import CustomButton from "./CustomButton"
-import Navbar from "./Navbar"
-import Footer from "./Footer"
-import SearchBar from "./SearchBar"
-import CustomFilter from "./CustomFilter"
-import SearchMenufacture from "./SearchMenufacture"
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchMenufacture from "./SearchMenufacture";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 export {
-    Hero,
-    CustomButton,
-    Navbar,
-    Footer,
-    SearchBar,
-    CustomFilter,
-    SearchMenufacture
-}
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchMenufacture,
+  CarCard,
+  CarDetails,
+};
