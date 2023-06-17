4f7c5d9494872d509b3b1a7bfdbb98697ae380a0
Sirajul islam <iamsiraj13@gmail.com>
2023-06-17 11:16:41 +0600

    hero section done

---
+++ types/index.ts
@@ -0,0 +1,16 @@
+import { manufacturers } from './../contstants/index';
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps{
+    title:string,
+    containerStyles?:string,
+    handleClick?:MouseEventHandler<HTMLButtonElement>,
+    btnType?:"button" | "submit"
+}
+
+
+export interface SearchManufacturerProps{
+    manufacturer:string,
+    setManufacturer: (manufacturer: string) => void;
+   
+}
